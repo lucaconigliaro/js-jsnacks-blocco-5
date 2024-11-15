1d5735264ad838bbb7e3459f09be74b7913104c0
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-15 13:22:40 +0100

    feat: snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authorPosts = posts.map(names => names.author);
+console.log(authorPosts);
