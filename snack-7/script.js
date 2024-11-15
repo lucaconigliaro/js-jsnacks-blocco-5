1e86bbac606ad676e762d967771f18a098595402
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-15 13:33:21 +0100

    feat: snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+let studentToFind = null;
+students.forEach((curStudent) => {
+  if (curStudent.id === 2) {
+    studentToFind = curStudent;
+  }
+});
+console.log(studentToFind);
